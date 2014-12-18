36e8c346e0ff63bcdecbdc0a1fc35d7d2fd39c21
kelthenoble <keltheceo@gmail.com>
2014-12-18 15:41:15 -0800

    Add devise and users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Blog::Application.configure do
+ Blog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -26,4 +26,5 @@ Blog::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
